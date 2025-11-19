fff3f4e722628225ce59fd4f24bc8d69c245c938
garvitj26-oss <garvitj26@gmail.com>
2025-11-19 14:52:32 +0530

    Add automatic location detection on initial page load

--- frontend/.next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++ frontend/.next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/app_globals_71f961d1.css",
-  "static/chunks/_8f45f48f._.js"
+  "static/chunks/_0b968fd1._.js"
 ],
     source: "dynamic"
 });
